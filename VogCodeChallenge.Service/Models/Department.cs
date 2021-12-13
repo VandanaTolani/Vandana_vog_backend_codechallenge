a3c4f57a7f59f204e747d00cb96e0bda271b95d0
VandanaTolani <vini.tolani.94@gmail.com>
2021-12-13 11:05:04 -0500

    added employee property

--- VogCodeChallenge.Service/Models/Department.cs
+++ VogCodeChallenge.Service/Models/Department.cs
@@ -1,9 +1,13 @@
-﻿namespace VogCodeChallenge.Service.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace VogCodeChallenge.Service.Models
 {
     public class Department
     {
         public long DepartmentId { get; set; }
         public string DepartmentName { get; set; }
         public Address Address { get; set; }
+        public IEnumerable<Employee>  Employees { get; set; }
     }
 }
